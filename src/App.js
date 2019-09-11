0db3592f9b2f129f656396fd6a3d2fbd8644d6ab
timurtimerhanov <timurgrunge@gmail.com>
2019-09-11 11:45:04 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          Timur 
+          <br/>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
